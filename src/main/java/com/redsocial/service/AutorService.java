591abfebbf9f09ef9766286589b09036efdda54e
Liliana <lilianacastillo071@hotmail.com>
2020-11-14 19:57:57 -0500

    Mi segundo examen

---
+++ src/main/java/com/redsocial/service/AutorService.java
@@ -0,0 +1,12 @@
+package com.redsocial.service;
+
+import java.util.List;
+
+import com.redsocial.entity.Autor;
+
+public interface AutorService {
+	
+	public abstract Autor inserta(Autor obj);
+	public abstract List<Autor> listaAutor();
+	
+}
